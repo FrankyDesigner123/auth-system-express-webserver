7cb29f303c1740d1983397632d856fb82ae9f528
Franky <62474988+ninja-uiux@users.noreply.github.com>
2020-11-20 22:54:55 +0100

    Login Route
    - Hash Password
    - Login Route
    - LoginValidation

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,9 @@ const express = require('express');
 // import comopnents from express-validator
 const { check, validationResult } = require('express-validator');
 
+// import bcrypt
+const bcrypt = require('bcryptjs');
+
 // init express.Router
 const router = express.Router();
 
@@ -11,8 +14,8 @@ const router = express.Router();
 // with this User model we can interact with database
 const User = require('../models/User');
 
-// create validation
-const validate = [
+// create registration validation
+const registerValidation = [
 	// define validation
 	check('fullName')
 		.isLength({ min: 2 })
@@ -23,8 +26,17 @@ const validate = [
 		.withMessage('Password must be at least 6 characters.'),
 ];
 
+// create login validation
+const loginValidation = [
+	// define validation
+	check('email').isEmail().withMessage('Please provide a valid email.'),
+	check('password')
+		.isLength({ min: 6 })
+		.withMessage('Password must be at least 6 characters.'),
+];
+
 // create the register route
-router.post('/register', validate, async (req, res) => {
+router.post('/register', registerValidation, async (req, res) => {
 	// check if req passes the validation
 	const errors = validationResult(req);
 
@@ -32,6 +44,12 @@ router.post('/register', validate, async (req, res) => {
 	if (!errors.isEmpty()) {
 		return res.status(422).json({ erros: errors.array() });
 	}
+
+	// generate a salt (random string)
+	const salt = await bcrypt.genSalt();
+
+	// make use of salt to hash password
+	const hashPassword = await bcrypt.hash(req.body.password, salt);
 
 	// check if userEmail exist
 	const userExist = await User.findOne({ email: req.body.email }); // check if email exist in database
@@ -44,7 +62,7 @@ router.post('/register', validate, async (req, res) => {
 		// set the propperties
 		fullName: req.body.fullName,
 		email: req.body.email,
-		password: req.body.password,
+		password: hashPassword,
 		role: req.body.role,
 	});
 
@@ -52,15 +70,40 @@ router.post('/register', validate, async (req, res) => {
 	try {
 		const savedUser = await user.save();
 		// send back savedUser
-		res.send(savedUser);
+		res.send({
+			// not good to send back savedUser object where password is in, so we send the prop we want to show
+			id: savedUser._id,
+			fullName: savedUser.fullName,
+			email: savedUser.email,
+		});
 	} catch (error) {
 		res.status(400).send(error);
 	}
 });
 
 // create the login route
-router.post('/login', (req, res) => {
-	res.send('Login Route.');
+router.post('/login', loginValidation, async (req, res) => {
+	// check if req passes the validation
+	const errors = validationResult(req);
+
+	// logic to check if there are errors (not passing validation)
+	if (!errors.isEmpty()) {
+		return res.status(422).json({ erros: errors.array() });
+	}
+
+	// check if email exist
+	const user = await User.findOne({ email: req.body.email });
+
+	if (!user) {
+		return res.status(404).send('User is not registered.');
+	}
+	// check if password correct
+	const validPassword = await bcrypt.compare(req.body.password, user.password);
+
+	if (!validPassword) {
+		return res.status(404).send('Invalid Email or Password');
+	}
+	return res.send('Logged in ...');
 });
 
 // export router
