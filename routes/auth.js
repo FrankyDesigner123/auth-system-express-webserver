9447df9687d724c5ebae73155c633aa078cb37e2
Franky <62474988+ninja-uiux@users.noreply.github.com>
2020-11-20 22:09:04 +0100

    Adding validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,9 @@
 // import express.
 const express = require('express');
 
+// import comopnents from express-validator
+const { check, validationResult } = require('express-validator');
+
 // init express.Router
 const router = express.Router();
 
@@ -8,8 +11,27 @@ const router = express.Router();
 // with this User model we can interact with database
 const User = require('../models/User');
 
+// create validation
+const validate = [
+	// define validation
+	check('fullName')
+		.isLength({ min: 2 })
+		.withMessage('Your full name is required.'),
+	check('email').isEmail().withMessage('Please provide a valid email.'),
+	check('password')
+		.isLength({ min: 6 })
+		.withMessage('Password must be at least 6 characters.'),
+];
+
 // create the register route
-router.post('/register', async (req, res) => {
+router.post('/register', validate, async (req, res) => {
+	// check if req passes the validation
+	const errors = validationResult(req);
+
+	if (!errors.isEmpty()) {
+		return res.status(422).json({ erros: errors.array() });
+	}
+
 	// create const of user and instance of this user model
 	const user = new User({
 		// set the propperties
