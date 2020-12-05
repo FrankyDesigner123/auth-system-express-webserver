86cc9d7fa31b8ac522bfba14d2c474d533f593d2
Franky <62474988+ninja-uiux@users.noreply.github.com>
2020-12-06 00:17:56 +0100

    Register user
    UI frontend,
    add useState hook,
    onChange function,
    onSubmit function

--- dashboard/src/components/auth/Register.js
+++ dashboard/src/components/auth/Register.js
@@ -1,8 +1,72 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const Register = (props) => {
-	return <div>Register</div>;
+	// useState hook to change input onChange
+	const [formData, setFormData] = useState({
+		fullName: '',
+		email: '',
+		password: '',
+		role: 'user',
+	});
+
+	const { fullName, email, password, role } = formData;
+
+	// onChange function to take input value from useState hook
+	const onChange = (e) =>
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+		console.log(formData);
+	};
+
+	return (
+		<Fragment>
+			<h1 className="large text-primary">Register Account</h1>
+			<p className="lead">
+				<i className="fas fa-user"></i>
+				{'  '}Create Your Account.
+			</p>
+			<form className="form" onSubmit={(e) => onSubmit(e)}>
+				<div className="form-group">
+					<input
+						type="text"
+						placeholder="Full Name"
+						name="fullName"
+						value={fullName}
+						onChange={(e) => onChange(e)}
+						required
+					/>
+				</div>
+				<div className="form-group">
+					<input
+						type="email"
+						placeholder="Email Address"
+						name="email"
+						value={email}
+						onChange={(e) => onChange(e)}
+						required
+					/>
+				</div>
+				<div className="form-group">
+					<input
+						type="password"
+						placeholder="Password"
+						name="password"
+						value={password}
+						onChange={(e) => onChange(e)}
+						minLength="6"
+					/>
+				</div>
+				<input type="submit" className="btn btn-primary" value="Register" />
+			</form>
+			<p className="my-1">
+				Already have an account? <Link to="/login">Login</Link>
+			</p>
+		</Fragment>
+	);
 };
 
 Register.propTypes = {};
