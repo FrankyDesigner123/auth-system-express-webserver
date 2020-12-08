e95f15fea1df2a2748dba42cb8e8ea433b58bae2
Franky <62474988+ninja-uiux@users.noreply.github.com>
2020-12-08 23:22:57 +0100

    Private Routing

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -11,6 +11,8 @@ import Landing from './components/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 // import actions
 import { loadUser } from './actions/auth';
@@ -35,6 +37,7 @@ const App = () => {
 						<Switch>
 							<Route exact path="/register" component={Register} />
 							<Route exact path="/login" component={Login} />
+							<PrivateRoute exact path="/dashboard" component={Dashboard} />
 						</Switch>
 					</section>
 				</Fragment>
